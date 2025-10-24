de7303b788153c22fabe7aaa8448f8465f71d28f
Xander Mulligan <xander.i.mulligan@gmail.com>
2025-10-24 17:01:41 -0600

    new project

---
+++ app/(tabs)/pleaseRename2.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const pleaseRename = () => {
+  return (
+    <View>
+      <Text>pleaseRename</Text>
+    </View>
+  )
+}
+
+export default pleaseRename
+
+const styles = StyleSheet.create({})
