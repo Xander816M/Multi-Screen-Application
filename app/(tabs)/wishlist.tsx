8f44d16012ad23c917627075c6501ca17a15f470
Abdullah <abdullahdauda55@gmail.com>
2025-10-28 17:02:35 -0600

    Add Top Bar (Wishlist and Limited Offers Pages)

--- app/(tabs)/wishlist.tsx
+++ app/(tabs)/wishlist.tsx
@@ -1,14 +1,52 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, TouchableOpacity, View, Image } from 'react-native'
 
 const Wishlist = () => {
   return (
+
     <View>
-      <Text>pleaseRename</Text>
+
+      {/* Top Bar */}
+      <View style={styles.topBar}>
+        
+        <Text style={styles.topBarContent}>Online + App-only Deals Until 10/30 | SHOP NOW</Text>
+
+          <TouchableOpacity>
+          <Image 
+            style={styles.topBarButton}
+            source={require("../../assets/images/right-arrow.png")}
+          />
+        </TouchableOpacity>
+          
+      </View>
+      
     </View>
   )
 }
 
 export default Wishlist
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+
+  topBar: {
+    borderWidth:1,
+    height: 85,
+    backgroundColor:"#000",
+    alignItems:"center",
+    justifyContent:"space-between",
+    flexDirection: "row",
+    paddingRight:10,
+    paddingLeft:10
+  },
+
+  topBarContent: {
+    color:"#fff",
+    marginTop:50,
+    fontSize:17,
+    
+    
+  },
+
+  topBarButton: {
+    marginTop:45
+  }
+})
